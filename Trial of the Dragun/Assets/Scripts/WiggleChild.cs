78ee19c30bd0eb52227eb1cb8d3af31954582a42
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-08-17 18:12:06 +0200

    Wiggle
    Dragon's parts can wiggle.

---
+++ Trial of the Dragun/Assets/Scripts/WiggleChild.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiggleChild : MonoBehaviour {
+
+	[SerializeField] float wiggleAmp;
+	[SerializeField] float wiggleTime;
+	float wiggleTimeFactor;
+
+	Transform parent;
+
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		time = 0;
+		wiggleTimeFactor = 2 * Mathf.PI / wiggleTime;
+		parent = this.transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveWiggle (time);
+		time += Time.deltaTime;
+	}
+
+	void MoveWiggle (float time)
+	{
+		float newY = parent.position.y + wiggleAmp * Mathf.Sin (time * wiggleTimeFactor);
+		float x = this.transform.position.x;
+		float z = this.transform.position.z;
+		Vector3 pos = new Vector3 (x, newY, z);
+
+		this.transform.position = pos; 
+	}
+
+}
