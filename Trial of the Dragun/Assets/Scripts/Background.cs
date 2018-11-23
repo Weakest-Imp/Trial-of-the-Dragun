fe316f166810a98f19956bf8b98d4787c7dfbb9c
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-11-23 16:56:16 +0100

    A LOT
    Brought back the background, sprites and basically all I could from the Jam project
    Created a movable Character and walls to block him
    Allowed changing speed for the background

---
+++ Trial of the Dragun/Assets/Scripts/Background.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	[SerializeField] float slideSpeed;
+	[SerializeField] float speedChangeTime;
+	float scale;
+
+	void Start () 
+	{
+		scale = this.transform.parent.localScale.x;
+	}
+
+	void Update () {
+		Slide ();
+	}
+	
+	// Slides the background at requested speed
+	void Slide () {
+		float x = this.transform.position.x + slideSpeed * Time.deltaTime;
+		float y = this.transform.position.y;
+		float z = this.transform.position.z;
+		Overflow (x, y, z);
+	}
+
+	// Teleports the background back once it goes out of screen
+	void Overflow (float x, float y, float z)
+	{
+		if (x > 5.25F * scale) {
+			this.transform.position = new Vector3 (x - 10.24F * scale, y, z);
+		} else {
+			if (x < -5.25F * scale) {
+				this.transform.position = new Vector3 (x + 10.24F * scale, y, z);
+			} else {
+				this.transform.position = new Vector3 (x, y, z);
+			}
+		}
+	}
+
+	public void speedChange (float newSpeed) {
+		StartCoroutine (speedChangeCoroutine(newSpeed));
+	}
+
+	IEnumerator speedChangeCoroutine (float newSpeed)
+	//Changes the speed smoothly
+	{
+		float change = newSpeed - slideSpeed;
+		float time = 0;
+		change = change / speedChangeTime;
+		while (time < speedChangeTime) {
+			yield return null;
+			slideSpeed += Time.deltaTime * change;
+			time += Time.deltaTime;
+		}
+		slideSpeed = newSpeed;
+	}
+}
---
+++ Trial of the Dragun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float playerSpeed;
+
+	private Rigidbody2D rb;
+
+	private float horInput;
+	private float verInput;
+	private float fireInput;
+
+
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+
+
+	void Update () {
+		checkInput ();
+
+		Move ();
+	}
+
+
+	//Genral Control for Scene Management________________________________________________________________________________________________________________________________________________________________
+	void checkInput () {
+		horInput = Input.GetAxis ("Horizontal");
+		verInput = Input.GetAxis ("Vertical");
+		fireInput = Input.GetAxis ("Fire1");
+	}
+
+	void resetInput () {
+		horInput = 0;
+		verInput = 0;
+		fireInput = 0;
+	}
+
+	public void playerDisable () {
+		this.enabled = false;
+		resetInput ();
+	}
+
+	public void playerEnable () {
+		this.enabled = true;
+	}
+
+	//Movement_____________________________________________________________________________________________________________________________________________________________________________________________
+	void Move () {
+		Vector2 move = new Vector2 (horInput, verInput);
+		if (move.magnitude > 1) {
+			move = move / move.magnitude;
+		}
+		//move.Normalize ();
+		rb.velocity = playerSpeed * move;
+	}
+
+}
