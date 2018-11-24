846e24f331760a9bb836390286e632d31e9bf8b6
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-11-24 19:51:25 +0100

    It does not move... Yet?
    Failed to implement a satisfactory pattern for the DraGun using Rigidbodies.
    It turns out it can work with the Transforms though...

--- Trial of the Dragun/Assets/Scripts/DraGun.cs
+++ Trial of the Dragun/Assets/Scripts/DraGun.cs
@@ -8,7 +8,7 @@ public class DraGun : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		head.PositionInitialize ();
 	}
 	
 	// Update is called once per frame
--- Trial of the Dragun/Assets/Scripts/WiggleChild.cs
+++ Trial of the Dragun/Assets/Scripts/WiggleChild.cs
@@ -6,8 +6,9 @@ public class WiggleChild : MonoBehaviour {
 
 	[SerializeField] float wiggleAmp = 1;
 	[SerializeField] float wiggleTime;
-	float wiggleTimeFactor;
 	[SerializeField] float wiggleOffSet;
+	float wiggleTimeFactor;
+	float wiggleVeloAmp;
 
 	[SerializeField] private Transform parent;
 	private Rigidbody2D parentRb;
@@ -20,10 +21,13 @@ public class WiggleChild : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody2D> ();
-		time = wiggleOffSet * Mathf.PI;
+		time = -1 * wiggleOffSet * Mathf.PI;
 		wiggleTimeFactor = 2 * Mathf.PI / wiggleTime;
+		wiggleVeloAmp = wiggleAmp * wiggleTimeFactor;
 		parentRb = parent.GetComponent<Rigidbody2D> ();
 		//parent = this.transform.parent;
+
+		//PositionInitialize ();
 	}
 	
 	// Update is called once per frame
@@ -42,11 +46,24 @@ public class WiggleChild : MonoBehaviour {
 
 	public void MoveWiggle (float deltaTime) {
 		time += deltaTime;
-		float wiggleSpeed = parentRb.velocity.y + wiggleAmp * Mathf.Cos (-1 * time * wiggleTimeFactor);
+		float wiggleSpeed = parentRb.velocity.y + wiggleVeloAmp * Mathf.Cos (-1 * time * wiggleTimeFactor);
 
 		rb.velocity = new Vector2 (0, wiggleSpeed);
 		if (!isFinal) {
 			child.MoveWiggle (deltaTime);
+		}
+	}
+
+	public void PositionInitialize () {
+		float pos = -1 * wiggleAmp * Mathf.Sin (wiggleTimeFactor * Mathf.PI * wiggleOffSet);
+		pos += parent.position.y;
+
+		float x = this.transform.position.x;
+		float z = this.transform.position.z;
+
+		this.transform.position = new Vector3 (x, pos, z);
+		if (!isFinal) {
+			child.PositionInitialize ();
 		}
 	}
 
