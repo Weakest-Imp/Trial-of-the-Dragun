21ae3f297dee33f250aa10e0390a985eaabdb635
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-11-24 19:52:04 +0100

    Revert "It does not move... Yet?"
    This reverts commit 846e24f331760a9bb836390286e632d31e9bf8b6.

--- Trial of the Dragun/Assets/Scripts/WiggleChild.cs
+++ Trial of the Dragun/Assets/Scripts/WiggleChild.cs
@@ -6,9 +6,8 @@ public class WiggleChild : MonoBehaviour {
 
 	[SerializeField] float wiggleAmp = 1;
 	[SerializeField] float wiggleTime;
-	[SerializeField] float wiggleOffSet;
 	float wiggleTimeFactor;
-	float wiggleVeloAmp;
+	[SerializeField] float wiggleOffSet;
 
 	[SerializeField] private Transform parent;
 	private Rigidbody2D parentRb;
@@ -21,13 +20,10 @@ public class WiggleChild : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody2D> ();
-		time = -1 * wiggleOffSet * Mathf.PI;
+		time = wiggleOffSet * Mathf.PI;
 		wiggleTimeFactor = 2 * Mathf.PI / wiggleTime;
-		wiggleVeloAmp = wiggleAmp * wiggleTimeFactor;
 		parentRb = parent.GetComponent<Rigidbody2D> ();
 		//parent = this.transform.parent;
-
-		//PositionInitialize ();
 	}
 	
 	// Update is called once per frame
@@ -46,24 +42,11 @@ public class WiggleChild : MonoBehaviour {
 
 	public void MoveWiggle (float deltaTime) {
 		time += deltaTime;
-		float wiggleSpeed = parentRb.velocity.y + wiggleVeloAmp * Mathf.Cos (-1 * time * wiggleTimeFactor);
+		float wiggleSpeed = parentRb.velocity.y + wiggleAmp * Mathf.Cos (-1 * time * wiggleTimeFactor);
 
 		rb.velocity = new Vector2 (0, wiggleSpeed);
 		if (!isFinal) {
 			child.MoveWiggle (deltaTime);
-		}
-	}
-
-	public void PositionInitialize () {
-		float pos = -1 * wiggleAmp * Mathf.Sin (wiggleTimeFactor * Mathf.PI * wiggleOffSet);
-		pos += parent.position.y;
-
-		float x = this.transform.position.x;
-		float z = this.transform.position.z;
-
-		this.transform.position = new Vector3 (x, pos, z);
-		if (!isFinal) {
-			child.PositionInitialize ();
 		}
 	}
 
